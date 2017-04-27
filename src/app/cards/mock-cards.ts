63b6759e20c067b11359dd3888fa3676f8f1a918
Tibo Brugidou <thibaud.brugidou@mondadori.fr>
2017-04-28 00:41:54 +0200

    style cards

--- src/app/cards/mock-cards.ts
+++ src/app/cards/mock-cards.ts
@@ -1,12 +1,12 @@
 import { Card } from './card';
 
 export const CARDS: Card[] = [
-	{name: "Illidan", type: "creature", cost: 4, attack: 7, defence: 7, picture: 'illidan.jpg'},
-	{name: "Arthas", type: "creature", cost: 4, attack: 4, defence: 8, picture: 'arthas.jpg'}, 
+	{name: "Illidan", type: "creature", cost: 8, attack: 7, defence: 7, picture: 'illidan.jpg'},
+	{name: "Arthas", type: "creature", cost: 7, attack: 4, defence: 8, picture: 'arthas.jpg'}, 
 	{name: "Sylvanas", type: "creature", cost: 4, attack: 6, defence: 5, picture: 'sylvanas.jpg'},
-	{name: "Thrall", type: "creature", cost: 4, attack: 6, defence: 5, picture: 'thrall.jpg'},
+	{name: "Thrall", type: "creature", cost: 2, attack: 6, defence: 5, picture: 'thrall.jpg'},
 	{name: "Kael Thas", type: "creature", cost: 4, attack: 6, defence: 5, picture: 'kael-thas.jpg'},
-	{name: "Cairne", type: "creature", cost: 4, attack: 6, defence: 5, picture: 'cairne.jpg'},
-	{name: "Jaina", type: "creature", cost: 4, attack: 6, defence: 5, picture: 'jaina.jpg'}, 
+	{name: "Cairne", type: "creature", cost: 5, attack: 6, defence: 5, picture: 'cairne.jpg'},
+	{name: "Jaina", type: "creature", cost: 6, attack: 6, defence: 5, picture: 'jaina.jpg'}, 
 	{name: "Leroy Jenkins", type: "creature", cost: 4, attack: 9, defence: 1, picture: 'leroy-jenkins.jpg'}
 ];
